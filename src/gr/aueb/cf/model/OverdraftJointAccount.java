8c3614e32c0f252e1e460eb035abe2d339b635fe
steliostsoupas <stelios-tsoupas@hotmail.com>
2023-06-14 19:16:10 +0300

    v0.1

---
+++ src/gr/aueb/cf/model/OverdraftJointAccount.java
@@ -0,0 +1,26 @@
+package gr.aueb.cf.model;
+
+import gr.aueb.cf.exceptions.NegativeAmountException;
+import gr.aueb.cf.exceptions.SsnNotValidException;
+
+public class OverdraftJointAccount extends JointAccount {
+
+    public OverdraftJointAccount() {}
+
+    public OverdraftJointAccount(User holder, String iban, double balance, User secondHolder) {
+        super(holder, iban, balance, secondHolder);
+    }
+
+    public void withdraw(double amount,String ssn)
+            throws SsnNotValidException, NegativeAmountException {
+        try {
+            if (amount < 0) throw new NegativeAmountException(amount);
+            if (!isSsnValid(ssn)) throw new SsnNotValidException(ssn);
+
+            setBalance(getBalance() - amount);
+        } catch (SsnNotValidException | NegativeAmountException e) {
+            System.out.println("Error: withdrawal");
+            throw e;
+        }
+    }
+}
